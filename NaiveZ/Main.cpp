3fc7c9820e418924d7f837014c6e9a3f30f81443
gzzouxiaohang <gzzouxiaohang@GIH-D-9568>
2016-11-05 14:13:09 +0800

    add glfw3.2

--- NaiveZ/Main.cpp
+++ NaiveZ/Main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
+#include "Third\glfw3.2\include\GLFW\glfw3.h"
+
 #include "File\Include\IOBJFileMgr.h"
 
 using namespace std;
-using namespace NaiveZ3D;
 
 int main()
 {
 	string file = "Res/EyeBall/eyeball.obj";
-	auto mgr = IOBJFileMgr();
+	auto mgr = NaiveZ3D::IOBJFileMgr();
 	auto model = mgr.Load(file);
 
 	system("pause");
